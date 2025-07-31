00fa90d033c1daf2e9de4e9dda9845e7bd8a747b
Tran Quang Nam <ksnam159@.yahoo.com>
2025-07-31 13:31:12 +0700

    Remove .buildozer and .gitmodules from repository

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/src/codecs/music_flac.o.d
+++
@@ -1,2 +0,0 @@
-/Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/src/codecs/music_flac.o: \
-  /Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_mixer/src/codecs/music_flac.c
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o.d
+++
@@ -1,8 +0,0 @@
-/Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o: \
-  /Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/enc/config_enc.c \
-  /Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h \
-  /Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h
-
-/Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h:
-
-/Users/trannam/Desktop/final_cnc_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h:
